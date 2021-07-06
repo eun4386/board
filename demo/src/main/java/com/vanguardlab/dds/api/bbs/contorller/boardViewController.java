255bd91c293d265db964ed9e93935ad56fd6cff3
seokhwahong <spn99176@gmail.com>
2021-07-06 09:06:45 +0900

    프로젝트 재구성

---
+++ demo/src/main/java/com/vanguardlab/dds/api/bbs/contorller/boardViewController.java
@@ -0,0 +1,15 @@
+package com.vanguardlab.dds.api.bbs.contorller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class boardViewController {
+	
+	@RequestMapping("/")
+	public String board() {
+		
+		return "list"; 
+	}
+	
+}
